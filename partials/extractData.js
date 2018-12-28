80fd046fd313245967b2b886ce914f6a8851ce22
jeroentvb <jeroen.van.berkum@hva.nl>
2018-12-28 17:07:42 +0100

    Added windguru temperature

--- partials/extractData.js
+++ partials/extractData.js
@@ -70,7 +70,8 @@ function windguruModel (number, $) {
     time: [],
     windspeed: [],
     windgust: [],
-    winddirection: []
+    winddirection: [],
+    temperature: []
   }
   // Get model name
   $(`#wgtab-obal-tabid_${number}`).find('.nadlegend').each(function () {
@@ -94,6 +95,11 @@ function windguruModel (number, $) {
   // Get winddirection
   $(`#tabid_${number}_0_SMER`).find('td span').each(function (i) {
     modelData.winddirection[i] = parseInt($(this).attr('title').match(/\d+/)[0]) - 180
+  })
+
+  // Get temperature
+  $(`#tabid_${number}_0_TMPE`).find('td').each(function (i) {
+    modelData.temperature[i] = $(this).text()
   })
 
   return modelData
