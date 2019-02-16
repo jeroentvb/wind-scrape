16b2c0b278d3cfceb5e82122d426221dd84f0fc8
Jeroen <36192730+jeroentvb@users.noreply.github.com>
2019-02-16 20:37:03 +0100

    Switch to puppeteer (#5)
    * Switch from nightmare to puppeteer
    
    * Cleaned the remains of nightmare
    
    * Added --no-sandbox flag
    
    * Added a line
    
    * Fixed waiting before a page is loaded - closes #3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const request = require('request')
-const Nightmare = require('nightmare')
+const puppeteer = require('puppeteer')
 const extract = require('./partials/extractData')
 
 function getHtml (url) {
@@ -17,7 +17,7 @@ function windfinder (spotname) {
     getHtml(`https://www.windfinder.com/weatherforecast/${spotname}`)
       .then(html => extract.windfinderData(html))
       .then(windfinder => {
-        if (windfinder.spot === '') reject(new Error(`The provided windfinder spot (${spotname}) doesn't exist..`))
+        if (windfinder.spot === '') reject(new Error('The provided windfinder spot doesn\'t exist..'))
         resolve(windfinder)
       })
       .catch(err => reject(err))
@@ -28,43 +28,58 @@ function windguru (spotnumber, modelNumbers) {
   if (!spotnumber) throw new Error('No spot number specified!')
   if (!modelNumbers) throw new Error('No model numbers specified!')
   if (!Array.isArray(modelNumbers)) throw new Error('Model numbers must be in an array!')
-  const nightmare = Nightmare({
-    show: false,
-    waitTimeout: 3000
-  })
+
   return new Promise((resolve, reject) => {
-    nightmare
-      .goto(`https://www.windguru.cz/${spotnumber}`)
-      .wait('.spot-name')
-      .wait('#tabid_2_0_dates')
-      .wait('#tabid_2_0_WINDSPD')
-      .wait('#tabid_2_0_GUST')
-      .wait('#tabid_2_0_SMER')
-      .evaluate(() => document.querySelector('body').outerHTML)
-      .end()
-      .then(html => extract.windguruData(html, modelNumbers))
-      .then(windguru => resolve(windguru))
-      .catch(err => {
-        if (err.message === '.wait() for .spot-name timed out after 3000msec') reject(new Error(`The provided windguru spot (number ${spotnumber}) doesn't exist..`))
-        reject(err)
+    (async function () {
+      const browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox'
+        ]
       })
+      const page = await browser.newPage()
+
+      try {
+        await page.goto(`https://www.windguru.cz/${spotnumber}`, { waitUntil: 'networkidle0' })
+
+        let html = await page.evaluate(() => document.body.innerHTML)
+        await browser.close()
+
+        let data = extract.windguruData(html, modelNumbers)
+        resolve(data)
+      } catch (err) {
+        await browser.close()
+        if (err.message === 'waiting for selector ".spot-name" failed: timeout 3000ms exceeded') reject(new Error('The provided windguru spot doesn\'t exist..'))
+        reject(err)
+      }
+    })()
   })
 }
 
 function windy (lat, long) {
   if (!lat || !long) throw new Error('No coordinates specified!')
-  const nightmare = Nightmare({
-    show: false
-  })
+
   return new Promise((resolve, reject) => {
-    nightmare
-      .goto(`https://www.windy.com/${lat}/${long}/wind?`)
-      .wait('.legend-left')
-      .evaluate(() => document.querySelector('body').outerHTML)
-      .end()
-      .then(html => extract.windyData(html))
-      .then(windy => resolve(windy))
-      .catch(err => reject(err))
+    (async function () {
+      const browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox'
+        ]
+      })
+      const page = await browser.newPage()
+
+      try {
+        await page.goto(`https://www.windy.com/${lat}/${long}/wind?`, { waitUntil: 'networkidle0' })
+
+        let html = await page.evaluate(() => document.body.innerHTML)
+        await browser.close()
+
+        let data = extract.windyData(html)
+        resolve(data)
+      } catch (err) {
+        await browser.close()
+        reject(err)
+      }
+    })()
   })
 }
 
