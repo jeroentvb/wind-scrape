cb5ec5383dfe1778e6174b261a593e82b31b77d2
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2019-11-24 11:52:25 +0100

    Refactored duplicate code in parseData.ts

--- src/partials/parseData.ts
+++ src/partials/parseData.ts
@@ -14,30 +14,9 @@ function windfinderData (data: ExtractedWindfinderData): WindfinderData {
   }
 
   const days: ParsedWindfinderDay[] = [
-    {
-      date: data.date[0],
-      time: utils.sliceDay.one(data.time),
-      windspeed: utils.sliceDay.one(data.windspeed),
-      windgust: utils.sliceDay.one(data.windgust),
-      winddirection: utils.sliceDay.one(data.winddirection),
-      temperature: utils.sliceDay.one(data.temperature)
-    },
-    {
-      date: data.date[1],
-      time: utils.sliceDay.two(data.time),
-      windspeed: utils.sliceDay.two(data.windspeed),
-      windgust: utils.sliceDay.two(data.windgust),
-      winddirection: utils.sliceDay.two(data.winddirection),
-      temperature: utils.sliceDay.two(data.temperature)
-    },
-    {
-      date: data.date[2],
-      time: utils.sliceDay.three(data.time),
-      windspeed: utils.sliceDay.three(data.windspeed),
-      windgust: utils.sliceDay.three(data.windgust),
-      winddirection: utils.sliceDay.three(data.winddirection),
-      temperature: utils.sliceDay.three(data.temperature)
-    }
+    utils.getWindfinderDay(data, 'one'),
+    utils.getWindfinderDay(data, 'two'),
+    utils.getWindfinderDay(data, 'three')
   ]
 
   days.forEach((day: ParsedWindfinderDay) => {
