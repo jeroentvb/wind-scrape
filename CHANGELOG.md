aa4302d5245e5754014131d7fe7260ce7de55963
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2022-02-27 21:02:28 +0000

    Update dist to output generated at cfc2e5f

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# v4.1.0
-* Add functionality to scrape a custom windguru spot
-* Add possibility to get a single windguru model
-
-# v4.0.0
-* Rename windfinder winddirection to winddirectionDegrees & winddirectionLetters
-* Remove url from windy output
--- dist/partials/url-builder.d.ts
+++
@@ -1,6 +0,0 @@
-export default class UrlBuilder {
-    static windfinder(spotname: string): string;
-    static windguru(spot: number | string): string;
-    static windy(lat: string | number, long: string | number): string;
-    static windReport(spotname: string): string;
-}
